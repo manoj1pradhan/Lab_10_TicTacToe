fb4e0baaac19e072527edb54d1538d7285481561
manoj1pradhan <pradhamj@mail.uc.edu>
2023-11-20 22:42:26 -0500

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+class TicTacToe {
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String[][] board = new String[ROW][COL];
+    private static String currentPlayer = "X";
+
+    public static void main(String[] args) {
+        clearBoard();
+        display();
+
+        while (true) {
+            getPlayerMove();
+            display();
+
+            if (isWin(currentPlayer)) {
+                System.out.println(currentPlayer + " wins!");
+                break;
+            }
+
+            if (isTie()) {
+                System.out.println("It's a tie!");
+                break;
+            }
+
+            togglePlayer();
+        }
+    }
+
+    private static void clearBoard() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                board[i][j] = " ";
+            }
+        }
+    }
+
+    private static void display() {
+        System.out.println("-------------");
+        for (int i = 0; i < ROW; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < COL; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+
+    private static void getPlayerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        do {
+            System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+        } while (!isValidMove(row, col));
+
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= ROW || col < 0 || col >= COL || !board[row][col].equals(" ")) {
+            System.out.println("Invalid move. Try again.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isWin(String player) {
+        return isRowWin(player) || isColWin(player) || isDiagonalWin(player);
+    }
+
+    private static boolean isRowWin(String player) {
+        for (int i = 0; i < ROW; i++) {
+            if (board[i][0].equals(player) && board[i][1].equals(player) && board[i][2].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isColWin(String player) {
+        for (int i = 0; i < COL; i++) {
+            if (board[0][i].equals(player) && board[1][i].equals(player) && board[2][i].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isDiagonalWin(String player) {
+        return (board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)) ||
+                (board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player));
+    }
+
+    private static boolean isTie() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (board[i][j].equals(" ")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void togglePlayer() {
+        currentPlayer = (currentPlayer.equals("X")) ? "O" : "X";
+    }
+}
